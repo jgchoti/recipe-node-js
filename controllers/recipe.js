140c38e35b8a44f20be76d3b35859419815318c2
jgchoti <jgchotirat@gmail.com>
2024-07-09 16:10:55 +0200

    Update recipe.js

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -12,7 +12,7 @@ const recipeControllers = {
         }
     },
 
-    getOneRecipe: async (req, res) => {
+    getRecipeByID: async (req, res) => {
         const { id } = req.params;
         try {
             let sql = 'SELECT * FROM recipes WHERE id = ?';
@@ -30,7 +30,8 @@ const recipeControllers = {
 
     getRecipeByName: async (req, res) => {
         const { q } = req.query;
-        let recipes;
+        let recipes = []
+        console.log(q);
         try {
             if (!q) {
                 recipes = await recipeControllers.getAllRecipes(req, res);
@@ -38,7 +39,6 @@ const recipeControllers = {
             const sql = 'SELECT * FROM recipes WHERE name LIKE ?';
             const searchTerm = `%${q}%`;
             recipes = await query(sql, [searchTerm]);
-            console.log(q);
             console.log(recipes);
             if (recipes.length === 0) {
                 res.status(404).json({ error: 'Recipe not found' });
@@ -50,9 +50,85 @@ const recipeControllers = {
             res.status(500).json({ error: 'Error fetching recipe' });
         }
     },
-    postRecipe: async (req, res) => { },
-    updateRecipe: async (req, res) => { },
-    deleteRecipe: async (req, res) => { },
+    postRecipe: async (req, res) => {
+        const { name, description } = req.body;
+        try {
+            if (!name) {
+                return res.status(400).json({ error: 'Name of the recipe is required' });
+            }
+
+
+            const sql = 'INSERT INTO recipes (name, description) VALUES(?, ?)';
+            const values = [name, description || null];
+
+            const recipes = await query(sql, values);
+            const insertedRecipeId = recipes.insertId;
+            console.log(insertedRecipeId)
+            const fetchSql = 'SELECT * FROM recipes WHERE id = ?';
+            const newRecipes = await query(fetchSql, [insertedRecipeId]);
+            res.status(201).json({
+                recipe: newRecipes[0], message: `"${newRecipes[0].name}" Recipe is added successfully`
+            });
+
+        } catch (error) {
+            console.error('Error adding recipe:', error);
+            res.status(500).json({ error: 'Error adding recipe' });
+        }
+    },
+    updateRecipe: async (req, res) => {
+        const { id } = req.params;
+        const { name, description } = req.body;
+        try {
+            if (!name && !description) {
+                return res.status(400).json({ error: 'At least one field (name or description) is required to update' });
+            }
+            let sql = 'UPDATE recipes SET ';
+            const values = [];
+            if (name) {
+                sql += 'name = ?, ';
+                values.push(name);
+            } if (description) {
+                sql += 'description = ?, ';
+                values.push(description);
+            }
+            sql = sql.slice(0, -2);
+            sql += ' WHERE id = ?';
+            values.push(id);
+            const recipe = await query(sql, values);
+            if (recipe.affectedRows === 0) {
+                return res.status(404).json({ error: 'Recipe not found' });
+            }
+            const fetchSql = 'SELECT * FROM recipes WHERE id = ?';
+            const updatedRecipe = await query(fetchSql, [id]);
+
+            res.status(200).json({ message: 'Recipe updated successfully', recipe: updatedRecipe[0] });
+        } catch (error) {
+            console.error('Error updating recipe:', error);
+            res.status(500).json({ error: 'Error updating recipe' });
+        }
+
+    },
+    deleteRecipe: async (req, res) => {
+        const { id } = req.params;
+        try {
+            const sql = 'SELECT * FROM recipes WHERE id = ?';
+            const selectRecipe = await query(sql, [id]);
+            if (selectRecipe.length === 0) {
+                return res.status(404).json({ error: 'Recipe not found' });
+            }
+            const deleteSql = 'DELETE FROM recipes WHERE id = ?';
+            await query(deleteSql, [id]);
+            const fetchSql = 'SELECT * FROM recipes';
+            const updatedRecipes = await query(fetchSql);
+            res.status(200).json({ message: `"${selectRecipe[0].name}" is deleted successfully`, recipeId: id, recipes: updatedRecipes });
+        }
+
+        catch (error) {
+            console.error('Error deleting recipe:', error);
+            res.status(500).json({ error: 'Error deleting recipe' });
+        }
+
+    }
 };
 
 export default recipeControllers;
