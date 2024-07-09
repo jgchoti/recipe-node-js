77d53380400c4acc30d5de1d4485d01633710c16
jgchoti <jgchotirat@gmail.com>
2024-07-09 13:26:31 +0200

    Update recipe.js

--- routes/recipe.js
+++ routes/recipe.js
@@ -6,10 +6,12 @@ const router = express.Router();
 
 
 // get recipes
-router.get('/', recipeControllers.getAllRecipes);
-router.get('/recipe/:id', recipeControllers.getOneRecipe);
-router.get('/recipe', recipeControllers.getRecipeByName);
-
+router.get('/recipes', recipeControllers.getAllRecipes);
+router.get('/recipes/:id', recipeControllers.getOneRecipe);
+router.get('/recipes', recipeControllers.getRecipeByName);
+router.post('/recipes', verifyToken, recipeControllers.postRecipe);
+router.put('/recipes/:id', verifyToken, recipeControllers.updateRecipe);
+router.delete('/recipes/:id', verifyToken, recipeControllers.deleteRecipe);
 // routes
 
 export default router;
