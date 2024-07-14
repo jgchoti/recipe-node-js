126803ef6144bec136b455e7f2fc1c7cf3cb8702
jgchoti <jgchotirat@gmail.com>
2024-07-14 16:13:41 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,13 @@ import userControllers from '../controllers/user.js';
 const router = express.Router();
 
 
-// routes
+// Route to render the login page
+router.get(['/', '/login'], (req, res) => {
+    res.render('login');
+});
+
+router.post('/register', userControllers.register);
+router.post('/login', userControllers.login);
+router.post('/logout', userControllers.logout);
 
 export default router;
