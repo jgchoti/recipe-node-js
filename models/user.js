d80c84faaf3e8280dceb8a9c85ce7e8e474e5fc4
jgchoti <jgchotirat@gmail.com>
2024-07-16 11:01:12 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,8 +1,19 @@
 import query from '../config/db.js';
 
 const createUserTable = async () => {
+    const sql = `
+        CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )
+    `;
     try {
-    } catch (err) {}
+        await query(sql)
+    } catch (err) { }
 };
 
 export default createUserTable;
